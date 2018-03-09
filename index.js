ae26641fa9ee5ffa4fed32d2ec03d5d58fb36f06
Daniel Marcoto <danielmarcoto@Mac-mini-de-Daniel.local>
2018-03-09 17:03:05 -0300

    Changes for using integrated with CodePipeline

--- index.js
+++ index.js
@@ -1,18 +1,60 @@
 'use strict';
 
 let AWS = require('aws-sdk');
-let s3 = new AWS.S3({apiVersion: '2006-03-01'});
 let Rx =  require('rx');
 let AdmZip = require('adm-zip');
-let bucket = 'my-bucket';
 let mime = require('mime-types');
 
+let s3 = new AWS.S3({apiVersion: '2006-03-01'});
+var codepipeline = new AWS.CodePipeline();
+
+// It`s up to you config a environment variable into the lambda update form
+let destinationBucket = process.env.DESTINATION_BUCKET || 'my-default-bucket';
+
 exports.handler = (event, context, callback) => {
-  let file_key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-  let params = { Bucket: bucket, Key: file_key };
+  var jobId = event["CodePipeline.job"].id;
+
+  // Notify AWS CodePipeline of a successful job
+  var putJobSuccess = function(message) {
+    var params = {
+        jobId: jobId
+    };
+    codepipeline.putJobSuccessResult(params, function(err, data) {
+        if(err) {
+            context.fail(err);      
+        } else {
+            context.succeed(message);      
+        }
+    });
+  };
+
+  // Notify AWS CodePipeline of a failed job
+  var putJobFailure = function(message) {
+    var params = {
+        jobId: jobId,
+        failureDetails: {
+            message: JSON.stringify(message),
+            type: 'JobFailed',
+            externalExecutionId: context.invokeid
+        }
+    };
+    codepipeline.putJobFailureResult(params, function(err, data) {
+        context.fail(message);      
+    });
+  };
+
+  // Here you take the ZIP file, generated from CodeBuild, for example
+  let originBucket = event["CodePipeline.job"].data.inputArtifacts[0].location.s3Location.bucketName;
+  let file_key = event["CodePipeline.job"].data.inputArtifacts[0].location.s3Location.objectKey;
+  
+  console.log(`BEFORE: Load ${originBucket}/${file_key}`)
+
+  let params = { Bucket: originBucket, Key: file_key };
   s3.getObject(params, (err, data) => {
     if (err) {
-      callback(err, null);
+      console.log(`ERR: onloading: ${originBucket}/${file_key}`)
+      
+      putJobFailure(err);
     } else {
       if (!data) callback(null, 'No Data!');
       let zip = new AdmZip(data.Body);
@@ -27,27 +69,29 @@ exports.handler = (event, context, callback) => {
             // If not, you may comment the following line in order to keep
             // the Content-disposition as 'attachment'
             ContentType : mime.lookup(zipEntry.name),
-            Bucket  : bucket,
+            Bucket  : destinationBucket,
             Key     : zipEntry.entryName, // Keeps the full path (folders)
             Body    : zipEntry.getData() // decompressed file as buffer
           };
+          console.log(`BEFORE: putObject ${destinationBucket}/${zipEntry.entryName}`)
           // upload decompressed file
           s3.putObject(params, (err, data) => {
-            if (err) console.log(err, err.stack); // an error occurred
+            if (err) putJobFailure(err); // an error occurred
             else results.push(data);           // successful response
           });
         },
         (err) => {
-          callback(err, null);
+          console.log(`ERR: onUploading:`)
+          putJobFailure(err);
         },
         () => {
-          let params = { Bucket: bucket, Key: file_key };
+          let params = { Bucket: originBucket, Key: file_key };
           // Delete zip file
           s3.deleteObject(params, (err, data) => {
             if (err) {
-              callback(err, null);
+              putJobFailure(err);
             } else {
-              callback(null, data);
+              putJobSuccess('Finished')
             }
           });
         }
